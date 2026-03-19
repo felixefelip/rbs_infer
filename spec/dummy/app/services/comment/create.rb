60309bf07e4e0cdb95d645600e6567cdff23fc06
Felipe Felix <felippe.felix98@gmail.com>
2026-03-19 03:29:03 -0300

    task hash_record_type_inference

--- spec/dummy/app/services/comment/create.rb
+++ spec/dummy/app/services/comment/create.rb
@@ -14,4 +14,19 @@ class Comment::Create
 		puts "Comment created successfully for comment #{comment.id}, status: #{status}"
 		comment.save!
 	end
+
+	def dummy_hash
+    { foo: "bar",
+		  baz: 42,
+	  	nested: { a: 1, b: 2 },
+			comment: Comment.new
+		}
+	end
+
+	def test_dummy_hash
+		dummy_hash[:comment].body = "Test body"
+
+		# adding a new key to the hash to test dynamic behavior
+		dummy_hash[:other_comment] = Comment.new(body: "Another comment")
+	end
 end
