d94f223becba3b83791f38488bfb23fdc7ffc451
Felipe Felix <felippe.felix98@gmail.com>
2026-07-18 20:06:01 -0300

    rubocop in tag_destroy

--- spec/dummy/app/services/tag_destroy.rb
+++ spec/dummy/app/services/tag_destroy.rb
@@ -3,128 +3,126 @@
 class TagDestroy
   attr_reader :tag, :user, :posts, :xml
 
-	def initialize(tag_id, user_id)
-		@tag = Tag.find(tag_id)
-		@posts = []
+  def initialize(tag_id, user_id)
+    @tag = Tag.find(tag_id)
+    @posts = []
     atribui_user(user_id)
-	end
+  end
 
-	def call
-		tag.destroy
-	end
+  def call
+    tag.destroy
+  end
 
   def call_xml
-	  test_nokogiri
-		parse_xml
-		parse_xml_as_hash
-		parse_xml_as_hash_with_parse
-		test_array_with_array
-		test_array_with_array_with_elements_untyped
+    test_nokogiri
+    parse_xml
+    parse_xml_as_hash
+    parse_xml_as_hash_with_parse
+    test_array_with_array
+    test_array_with_array_with_elements_untyped
   end
 
-	private
+  private
 
-	def atribui_user(user_id)
-		@user = User.find(user_id)
-	end
+  def atribui_user(user_id)
+    @user = User.find(user_id)
+  end
 
   def user_name
-		user.name
+    user.name
   end
 
-	def iterate_tags
-		my_tags = tag.posts.first&.tags&.order(:name)
-		my_tags&.each do |t|
-			t.save!
-		end
-	end
+  def iterate_tags
+    my_tags = tag.posts.first&.tags&.order(:name)
+    my_tags&.each do |t|
+      t.save!
+    end
+  end
 
-	# Test when the local var has the same name as the attr_reader and different type
+  # Test when the local var has the same name as the attr_reader and different type
   def iterate_tag_posts
-	  posts = tag.posts.order(:created_at)
-	  posts.where(status: :published).each do |post|
-	    puts post.title
-	  end
+    posts = tag.posts.order(:created_at)
+    posts.where(status: :published).each do |post|
+      puts post.title
+    end
   end
 
   def iterate_tag_posts_with_transaction
-		ActiveRecord::Base.transaction do
-			iterate_tag_posts
-		end
+    ActiveRecord::Base.transaction do
+      iterate_tag_posts
+    end
   end
 
   def call_process_tag
-	  process_tag(tag)
+    process_tag(tag)
   end
 
   def process_tag(tag)
-	  tag.save!
+    tag.save!
   end
 
-	def test_nokogiri
-		@xml = Nokogiri::XML("")
-	end
+  def test_nokogiri
+    @xml = Nokogiri::XML('')
+  end
 
   def parse_xml
-		xml.xpath("//Pedidos").map do |order|
+    xml.xpath('//Pedidos').map do |order|
       order
     end
   end
 
-	def parse_xml_as_hash
-    xml.xpath("//Pedidos").map do |order_xml|
+  def parse_xml_as_hash
+    xml.xpath('//Pedidos').map do |order_xml|
       test_hash(order_xml)
     end
-	end
+  end
 
-	def parse_xml_as_hash_with_parse
-		xml.xpath("//Pedidos").map do |order_xml|
-			ParseXml.new(order_xml).parse
-		end
-	end
+  def parse_xml_as_hash_with_parse
+    xml.xpath('//Pedidos').map do |order_xml|
+      ParseXml.new(order_xml).parse
+    end
+  end
 
-	def test_hash(order_xml)
-		{ order: order_xml, date: order_xml.at_css("date") }
-	end
+  def test_hash(order_xml)
+    { order: order_xml, date: order_xml.at_css('date') }
+  end
 
-	def test_array_with_array
-		xml.xpath("//Pedidos").map do |order_xml|
-			[order_xml, order_xml.at_css("date")]
-		end
-	end
+  def test_array_with_array
+    xml.xpath('//Pedidos').map do |order_xml|
+      [order_xml, order_xml.at_css('date')]
+    end
+  end
 
-	def test_array_with_array_with_elements_untyped
-		xml.xpath("//Pedidos").map do |order_xml|
-			[untyped]
-		end
-	end
+  def test_array_with_array_with_elements_untyped
+    xml.xpath('//Pedidos').map do |_order_xml|
+      [untyped]
+    end
+  end
 
-	def verify_multiples_returns_with_void
-		if user_posts.any?
-			save_post
-		elsif user_name
-		 	user_name
-		else
-			nil
-		end
-	end
+  def verify_multiples_returns_with_void
+    if user_posts.any?
+      save_post
+    elsif user_name
+      user_name
+    end
+  end
 
-	def verify_multiples_returns_with_void_and_rescue
-		user_posts.first!.save!
-	rescue StandardError
-		"error"
-	end
-
-	# when returns two types
-	# def verify_multiples_returns
-	# 	user_posts.first!.save || user_name
-	# end
-
-	def save_post
+  def verify_multiples_returns_with_void_and_rescue
     user_posts.first!.save!
-	end
+  rescue StandardError
+    'error'
+  end
 
-	def user_posts
+  # when returns two types
+  # def verify_multiples_returns
+  # 	user_posts.first!.save || user_name
+  # end
+
+  def save_post
+    user_posts.first!.save!
+  end
+
+  def user_posts
     @user.posts
-	end
+  end
 end
