06d02374f0d1068047e6aa078a866e7d0e45c775
Felipe Felix <felippe.felix98@gmail.com>
2026-07-18 23:52:57 -0300

    Improve example 4

--- spec/dummy/app/models/example4.rb
+++ spec/dummy/app/models/example4.rb
@@ -1,18 +1,26 @@
 class Example4
-	class Foo
+  class Foo
     def self.name
 		  @name
     end
 
-		def self.name=(value)
+    def self.name=(value)
 	    @name= value
-		end
-	end
+    end
+  end
 
-	def run
-		Foo.name.upcase # error not method
+  class Bar
+	  def foo_name
+		  Foo.name
+	  end
+  end
+
+  def run
+    Foo.name.upcase # error not method
 
 		Foo.name = 'John Doe'
-		Foo.name.upcase # => "JOHN DOE"
-	end
+    Foo.name.upcase # => "JOHN DOE"
+
+    Bar.new.foo_name.upcase # => "JOHN DOE"
+  end
 end
