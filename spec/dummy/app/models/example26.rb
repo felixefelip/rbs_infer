c1a75323e6c5e598a8f3b11cc4ca5fe703827b76
Felipe Felix <felippe.felix98@gmail.com>
2026-08-12 14:39:55 -0300

    example23

---
+++ spec/dummy/app/models/example26.rb
@@ -0,0 +1,42 @@
+class Example25
+  module Foo
+    def bazinga(module_included)
+      module_included.bazingado(self)
+    end
+
+    def bazingado(base_foo)
+      puts "base_foo class: #{base_foo.class}"
+    end
+  end
+
+  module Baz
+    extend Example25::Foo
+  end
+
+  module BazOther
+    extend Example25::Foo
+
+    def self.bazingado(base_foo)
+      base_foo.log_something("bazingado")
+    end
+  end
+
+  class Bar
+    extend Example25::Foo
+
+    # Runs at class-body time, so `Baz` above has to be defined already.
+    bazinga(Example25::Baz)
+  end
+
+  class BarOther
+    extend Example25::Foo
+
+    def self.log_something(message)
+      puts message
+
+      message
+    end
+
+    bazinga(Example25::BazOther)
+  end
+end
