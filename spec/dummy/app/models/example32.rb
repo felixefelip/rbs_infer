a4b834850fac9c7abdd0e42973cd27db944ec933
Felipe Felix <felippe.felix98@gmail.com>
2026-08-15 14:27:05 -0300

    example32

---
+++ spec/dummy/app/models/example32.rb
@@ -0,0 +1,31 @@
+class Example32
+  module Foo
+    def bazinga(module_included)
+      module_included.bazingado(self)
+    end
+
+    def bazingado(base=nil, &block)
+      if base.nil?
+        @_bazingado_block = block
+      else
+        base.class_eval(&@_bazingado_block) if @_bazingado_block
+      end
+    end
+  end
+
+  module Baz
+    extend Example32::Foo
+
+    bazingado do
+      def age
+        31
+      end
+    end
+  end
+
+  class Bar
+    extend Example32::Foo
+
+    bazinga(Example32::Baz)
+  end
+end
