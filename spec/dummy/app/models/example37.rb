e0853d4e4b6bbb29757388201303278e01f45e50
Felipe Felix <felippe.felix98@gmail.com>
2026-08-16 02:26:02 -0300

    example37

---
+++ spec/dummy/app/models/example37.rb
@@ -0,0 +1,37 @@
+class Example37
+  module Foo
+    def bazinga(*modules)
+      modules.reverse_each do |mod|
+        mod.bazingado(self)
+      end
+    end
+
+    def bazingado(base=nil, &block)
+      if base.nil?
+        @_bazingado_block = block
+      else
+        base.class_eval(&@_bazingado_block) if @_bazingado_block
+      end
+    end
+  end
+
+  class Baz
+    extend Example37::Foo
+
+    bazingado do
+      def age
+        31
+      end
+    end
+  end
+
+  class Bar
+    extend Example37::Foo
+
+    bazinga(Example37::Baz)
+
+    def call_age
+      age + 10
+    end
+  end
+end
