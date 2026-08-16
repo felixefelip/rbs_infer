0ecc21cd8ddf84379a2d37b2c2ce521ced6395bc
Felipe Felix <felippe.felix98@gmail.com>
2026-08-16 18:27:25 -0300

    example41

---
+++ spec/dummy/app/models/example41.rb
@@ -0,0 +1,27 @@
+class Example41
+  module Foo
+    def included(base=nil, &block)
+      if base.nil?
+        @_bazingado_block = block
+      else
+        base.class_eval(&@_bazingado_block) if @_bazingado_block
+      end
+    end
+  end
+
+  module Baz
+    extend Example41::Foo
+
+    included do
+      def age
+        31
+      end
+    end
+  end
+
+  class Bar
+    extend Example41::Foo
+
+    include(Example41::Baz)
+  end
+end
